d54dff9ec99c066de7d28d715260a3cd791e0cf7
wjae <279293903@qq.com>
2019-07-12 11:27:46 +0800

    修改翻页

--- 壹线心理/js/index.js
+++ 壹线心理/js/index.js
@@ -1,54 +1,20 @@
-var mySwiper = new Swiper('.swiper-container1', {
-    autoplay: 3000,
-    loop: true,
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-});
-
-var swiper = new Swiper('.swiper-container2', {
-    slidesPerView: 2.5,
-    spaceBetween: 10,
-    freeMode: true
-});
-
 $(function () {
-
-    // 分类页面左侧导航
-    $("#tab2 .left li").click(function () {
-        $(".sort").hide();
-        $(this).addClass("on").siblings().removeClass("on");
+    var mySwiper = new Swiper('.swiper-container1', {
+        autoplay: 3000,
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
     });
 
-    // 排序弹框
-    $(".sortBtn").click(function () {
-        if ($(".sort").is(":hidden")) {
-            $(".sort").show();
-            $(".sortBtn img").css({ "transform": "rotate(180deg)" });
-        } else {
-            $(".sort").hide();
-            $(".sortBtn img").css({ "transform": "rotate(0deg)" });
-        }
+    var swiper = new Swiper('.swiper-container2', {
+        slidesPerView: 2.5,
+        spaceBetween: 10,
+        freeMode: true
     });
-    $(".sort").click(function () {
-        $(this).hide();
-    })
-    $(".sort ul").click(function () {
-        return false;
-    });
-    $(".sort li").click(function () {
-        $(this).addClass("on").siblings().removeClass("on");
-    })
 
-    // 底部的切换
-    // $(".weui-tabbar a").click(function () {
-    //     $(this).addClass("weui-bar__item--on").siblings().removeClass("weui-bar__item--on");
-    //     var li_index = $(".weui-tabbar a").index(this);
-    //     $(".weui-tab__bd-item").eq(li_index).fadeIn(0).siblings('.weui-tab__bd-item').hide();
-    //     swiperFn();
-    // })
+
 
     // // 好文页面
-    // function swiperFn() {
     var mySwiperTop = new Swiper('#topNav', {
         freeMode: true,
         freeModeMomentumRatio: 0.5,
@@ -79,17 +45,10 @@ $(function () {
         $("#topNav .active").removeClass('active');
         $("#topNav .swiper-slide").eq(swiper.clickedIndex).addClass('active');
         $(".tab-list").eq(swiper.clickedIndex).fadeIn().siblings('.tab-list').hide();
+
+        var curNavIndex = swiper.clickedIndex;
+        console.log(curNavIndex);
+
+        
     });
-    // }
-
-
-
-    // 我的页面的跳转
-    // $(".allTest").click(function () {
-    //     $("#tab4").removeClass("weui-tab__bd-item--active");
-    //     $("#tab2").addClass("weui-tab__bd-item--active");
-
-    //     $(".weui-tabbar a").eq(3).removeClass("weui-bar__item--on");
-    //     $(".weui-tabbar a").eq(1).addClass("weui-bar__item--on");
-    // });
-});
+})
